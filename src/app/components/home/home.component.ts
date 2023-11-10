88706f121dbba9f8b852b8048ae3fc7157a1ac0d
semyrekov.vladyslav <semyrekov.vladyslav@gmail.com>
2023-11-10 12:14:48 +0200

    tile close items

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -170,8 +170,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
           title: location.waveLocationName,
           name: location.waveLocationKey,
           reflink: location.waveLocationReferralLink,
-          status: ['planned', 'permanently closed', 'cancelled'].includes(specifications[0].waveSpecificationStatus.toLowerCase())
-            && this.isOpenedLocation(specifications[0].waveSpecificationCommissioningDate) ? 'closed' : 'opened',
+          status: ['planned', 'permanently closed', 'cancelled'].includes(specifications[0].waveSpecificationStatus.toLowerCase()) || !this.isOpenedLocation(specifications[0].waveSpecificationCommissioningDate) || (['open only summer season'].includes(specifications[0].waveSpecificationStatus.toLowerCase()) && this.isSummer) ? 'closed' : 'opened',
           options: {
             zIndex: location.waveLocationAffiliate ? 99 : null,
             label: location.waveLocationAffiliate ? {className: 'affiliate-za', text: 'a'} : '',
